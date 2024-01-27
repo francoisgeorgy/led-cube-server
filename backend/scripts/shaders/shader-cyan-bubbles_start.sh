23836b3ae3c7ca00b4ac181059dbc11ea283e57d
François Georgy <francois.georgy@gmail.com>
2024-01-27 18:59:36 +0100

    shaders: use absolute path for shady (because of sudo)

--- backend/scripts/shaders/shader-cyan-bubbles_start.sh
+++ backend/scripts/shaders/shader-cyan-bubbles_start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-/home/cube/glsl/run.sh /home/cube/glsl/scripts/cyan-bubbles.glsl
+#/home/cube/glsl/run.sh /home/cube/glsl/scripts/cyan-bubbles.glsl
+
+export EGL_PLATFORM=surfaceless
+export MESA_GL_VERSION_OVERRIDE=3.3
+
+LEDCAT="/home/cube/rpi-rgb-led-matrix/examples-api-use/ledcat"
+SHADY="/home/francois/.local/share/go/bin/shady"
+
+script="/home/cube/glsl/scripts/cyan-bubbles.glsl"
+
+$SHADY -ofmt rgb24 -g 192x128 -f 20 -i "${script}" \
+    | sudo $LEDCAT \
+        --led-rows=64 --led-cols=64 --led-slowdown-gpio=5 \
+        --led-parallel=2 --led-chain=3 --led-brightness=65
