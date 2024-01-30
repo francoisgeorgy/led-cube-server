f8cb9ab36ecb3c04effae30d9671a181acf75967
François Georgy <francois.georgy@gmail.com>
2024-01-30 22:19:50 +0100

    Fix startup script

--- backend/scripts/applications/dice_start.sh
+++ backend/scripts/applications/dice_start.sh
@@ -9,7 +9,7 @@ sudo -E env PATH=$PATH python src/samples/${APP} \
     --led-slowdown-gpio 5 \
     --led-rows=64 --led-cols=64 \
     --led-chain 3 --led-parallel 2 \
-    --led-brightness 30 \
+    --led-brightness 50 \
     --led-pixel-mapper=Cube
 
 #echo $! | sudo tee /home/cube/python/websockets_color.pid
